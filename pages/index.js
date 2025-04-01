bf1dfef29205a7f1e590e220de938b9f04c66fb4
murillodominguez <murillo.v.carballo@gmail.com>
2025-03-31 22:26:56 -0300

    testing static page building for data update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-function Header(props) {
+import Link from 'next/link'
+
+export function Header(props) {
     return <h1>{props.title}</h1>
 }
 
-function HomePage() {
-    return <Header title="Hello world!"></Header>
+export default function HomePage() {
+    return <div><Header title="Hello world!"></Header>
+    <Link href="/sobre">
+    Sobre
+    </Link></div>
 }
-
-export default HomePage;
---
+++ pages/sobre.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import {Header} from './index';
+
+function Sobre() {
+    return <div><Header title="Sobre"></Header>
+    <Link href="/">
+        Home
+    </Link></div>
+}
+
+export default Sobre
